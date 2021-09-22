4ec9eff8a6c150c994dd3e70fd736f4509e22e13
tbenedek92 <t.benedek92@gmail.com>
2021-09-22 19:37:33 +0200

    Added /metrics for prometheus monitoring

--- app.py
+++ app.py
@@ -2,8 +2,16 @@ from flask import Flask, redirect, url_for
 import os
 from weather import Weather
 import geocoder
+from werkzeug.middleware.dispatcher import DispatcherMiddleware
+from prometheus_client import make_wsgi_app, Gauge
+import time
+import threading
 
+API_KEY = os.environ.get('OPEN_WEATHER_API_KEY')
 app = Flask(__name__)
+app.wsgi_app = DispatcherMiddleware(app.wsgi_app, {
+    '/metrics': make_wsgi_app()
+})
 
 
 @app.route('/')
@@ -13,9 +21,9 @@ def hello_world():
 
 @app.route('/weather/<city>')
 def city_weather(city):
-    api_key = os.environ.get('OPEN_WEATHER_API_KEY')
-    if api_key is not None:
-        weather = Weather(api_key=api_key, city=city)
+
+    if API_KEY is not None:
+        weather = Weather(api_key=API_KEY, city=city)
         return f'The temperature in {str(city).title()} is {weather.get_temp_c()} ºC, ' \
                f'which equals to {weather.get_temp_f()} F.'
     else:
@@ -28,5 +36,27 @@ def weather_redirect():
     return redirect(url_for(f'city_weather', city=g.city))
 
 
+def set_metrics(city):
+    weather = Weather(api_key=API_KEY, city=city)
+    return weather.get_temp_c()
+
+
+def define_weather_metrics(frequency=30):
+    city_list = ["Zurich", "Budapest"]
+    gauge_dict = {}
+    for city in city_list:
+        gauge = Gauge(f'{str(city).lower()}_c', f'Current temperature in C in {str(city).title()}')
+        gauge_dict[city] = gauge
+    while True:
+        for city, gauge in gauge_dict.items():
+            try:
+                weather = Weather(api_key=API_KEY, city=city)
+                gauge.set_function(weather.get_temp_c)
+            except ConnectionError:
+                continue
+        time.sleep(frequency)
+
+
 if __name__ == '__main__':
+    threading.Thread(target=define_weather_metrics, args=[60]).start()
     app.run(host='0.0.0.0')

6a9a7cb09e037009fffeea70e59495b57bdd3622
tbenedek92 <t.benedek92@gmail.com>
2021-09-23 20:50:19 +0200

    Initial commit of the empty branch

--- Dockerfile
+++ Dockerfile
@@ -1,8 +1,5 @@
 FROM python:3.9-slim-bullseye
 
-ARG OPEN_WEATHER_API_KEY="Default_Value"
-ENV OPEN_WEATHER_API_KEY=${OPEN_WEATHER_API_KEY}
-
 WORKDIR /app
 COPY . .
 RUN pip3 install -r requirements.txt
--- app.py
+++ app.py
@@ -1,7 +1,4 @@
-from flask import Flask, redirect, url_for
-import os
-from weather import Weather
-import geocoder
+from flask import Flask
 
 app = Flask(__name__)
 
@@ -9,23 +6,6 @@ app = Flask(__name__)
 @app.route('/')
 def hello_world():
     return '<p>Hello, World!</p>'
-
-
-@app.route('/weather/<city>')
-def city_weather(city):
-    api_key = os.environ.get('OPEN_WEATHER_API_KEY')
-    if api_key is not None:
-        weather = Weather(api_key=api_key, city=city)
-        return f'The temperature in {str(city).title()} is {weather.get_temp_c()} ºC, ' \
-               f'which equals to {weather.get_temp_f()} F.'
-    else:
-        return 'No API key available for connecting to Open Weather'
-
-
-@app.route('/weather')
-def weather_redirect():
-    g = geocoder.ip('me')
-    return redirect(url_for(f'city_weather', city=g.city))
 
 
 if __name__ == '__main__':
